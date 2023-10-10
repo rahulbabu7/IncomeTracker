f2fcc4b99163344bb99d99385ee3545752c89b8e
rahulbabu7 <rahulbabu436@gmail.com>
2023-10-10 22:19:42 +0530

    added total income

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive,computed } from 'vue';
+import Header from './components/Header.vue';
+const state = reactive({
+  income:[2000,1000,2000],
+  totalIncome :computed(()=>{
+    let sum = 0;
+    if(state.income.length >0){
+      //method 1 
+      // for(let i =0;i<state.income.length;i++){
+      //  sum += state.income[i];
+      
+      // }
+      //! In for of loop the loop variable consist of the  array
+      for (const i  of state.income) {
+        sum +=i
+      }
+      return sum
+    }
+  })
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<Header :totalIncome ="state.totalIncome" />  
+<!-- passing the total income to the header component -->
+<h1>{{ state.totalIncome }}</h1>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing:border-box;
+  font-family: 'Fira Sans' ,sans-serif;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+body{
+  background: #EEE;
 }
 </style>
